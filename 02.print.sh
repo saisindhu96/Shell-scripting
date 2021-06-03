3c51e7312fab53353822423df09286bb87ff9a53
saisindhu96 <saisindhuraju@gmail.com>
2021-06-03 20:47:15 +0530

    Added new line comment

--- 02.print.sh
+++ 02.print.sh
@@ -1,7 +1,11 @@
-echo and prinf commands are used to print some message on the screen
+#echo and printf commands are used to print some message on the screen
 
 #How ever we used echo command here , because it is simple and yet effective to all the needs
 
 # syntax: echo Input Message
 
 echo Hello world
+
+#echo to print multiple lines (\n)
+
+echo -e "Hello,\nWelcome to my Github"
