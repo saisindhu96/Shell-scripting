2a87cbcf1354d63fde60efba3477faf079db2930
saisindhu96 <saisindhuraju@gmail.com>
2021-06-03 20:34:56 +0530

    Added print.sh file

---
+++ 02.print.sh
@@ -0,0 +1,7 @@
+echo and prinf commands are used to print some message on the screen
+
+How ever we used echo command here , because it is simple and yet effective to all the needs
+
+# syntax: echo Input Message
+
+echo Hello world
