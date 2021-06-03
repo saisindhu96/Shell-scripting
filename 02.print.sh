15e2ab6a7c348e4963f8dddb2edbf51bc436dce4
saisindhu96 <saisindhuraju@gmail.com>
2021-06-03 21:05:28 +0530

    Added new line comment

--- 02.print.sh
+++ 02.print.sh
@@ -11,3 +11,21 @@ echo Hello world
 echo -e "Hello,\nWelcome to my Github\n@saisindhu"
 
 echo -e "Iam\tsindhu\tworking\ton\tproject"
+
+##print text in colors
+#syntax : echo -e "\e[COLORmMessage"
+
+#Colors         #code
+#RED             31
+#GREEN           32
+#YELLOW          33
+#BLUE            34
+MAGENETA         35
+CYAN             36
+
+echo -e "\e[31 Hello In RED"
+echo -e "\e[32 Hello In GREEN"
+echo -e "\e[33 Hello In YELLOW"
+echo -e "\e[34 Hello In BLUE"
+echo -e "\e[35 Hello In MAGENETA"
+echo -e "\e[36 Hello In CYAN"
