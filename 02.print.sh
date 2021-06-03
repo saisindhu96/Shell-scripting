0562d005f4991ec5ad95149b0fe0f0ed446aecac
saisindhu96 <saisindhuraju@gmail.com>
2021-06-03 21:07:30 +0530

    Added new line comment

--- 02.print.sh
+++ 02.print.sh
@@ -20,12 +20,12 @@ echo -e "Iam\tsindhu\tworking\ton\tproject"
 #GREEN           32
 #YELLOW          33
 #BLUE            34
-MAGENETA         35
-CYAN             36
+#MAGENETA         35
+#CYAN             36
 
-echo -e "\e[31 Hello In RED"
-echo -e "\e[32 Hello In GREEN"
-echo -e "\e[33 Hello In YELLOW"
-echo -e "\e[34 Hello In BLUE"
-echo -e "\e[35 Hello In MAGENETA"
-echo -e "\e[36 Hello In CYAN"
+echo -e "\e[31mHello In RED"
+echo -e "\e[32mHello In GREEN"
+echo -e "\e[33mHello In YELLOW"
+echo -e "\e[34mHello In BLUE"
+echo -e "\e[35mHello In MAGENETA"
+echo -e "\e[36mHello In CYAN"
