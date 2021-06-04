467e5c9e7fc9ae4b21bf85413eda75d681897ab6
saisindhu96 <saisindhuraju@gmail.com>
2021-06-04 19:44:13 +0530

    Added

--- 03.variable.sh
+++ 03.variable.sh
@@ -10,3 +10,6 @@ echo Course Name = $COURSE
 #DATE=2021-06-04
 DATE=$(date +%F)
 echo Good Evening, Today date is $DATE
+
+NO_OF_USERS=$(who | wc -l)
+echo number of users logged in system = $NO_OF_USERS
