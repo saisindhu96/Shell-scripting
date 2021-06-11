0a81bd0e0f8982cfc80596b1e85fb9863dfc7a59
saisindhu96 <saisindhuraju@gmail.com>
2021-06-11 21:14:16 +0530

    added frontend  file

---
+++ roboshop/components/mongodb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source components/common.sh
+rm -f /tmp/roboshop.log
+set-hostname mongodb
+
+HEAD "setup MongoDB yum repo file\t"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT $?
+
+HEAD "Install mondoDB\t\t"
+yum install -y mogodb-org &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Update the List address in config file"
+sed -e -i 's/127.0.0.1/ 0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+HEAD "Start MongoDB Service\t\t"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Download Schema from GitHub\t"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract Downloaded Archive\t"
+cd /tmp
+unzip -o mongodb.zip &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Load Schema\t\t\t"
+cd mongodb-main
+mongo < catalogue.js &>>/tmp/roboshop.log && mongo < users.js &>>/tmp/roboshop.log
+STAT $?
