8ccfcdad51d9317c2e4ac06ab00152d5bae71a0a
saisindhu96 <saisindhuraju@gmail.com>
2021-06-12 19:38:53 +0530

    added frontend  file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -12,17 +12,17 @@ HEAD "Download from GitHub\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Delete old HTML DOCS\t"
+HEAD "Delete old HTML DOCS\t\t\t"
 rm -rf /usr/share/nginx/html/*
 STAT $?
 
-HEAD "Exract Download Content"
+HEAD "Exract Download Content\t\t"
 unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
 mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Update nginx configuration"
+HEAD "Update nginx configuration\t\t"
 mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT $?
 
@@ -32,7 +32,7 @@ for component in catalogue cart user shipping payment ; do
 done
 STAT $?
 
-HEAD "start Nginx\t\t"
+HEAD "start Nginx\t\t\t\t"
 systemctl restart nginx &>>/tmp/roboshop.log
 systemctl enable nginx &>>/tmp/roboshop.log
 STAT $?
