a52b124dc23afc76818c4e36d1f04dd6431d8f88
saisindhu96 <saisindhuraju@gmail.com>
2021-06-13 20:30:35 +0530

    added

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,4 +2,34 @@
 
 source components/common.sh
 rm -f /tmp/roboshop.log
-set-hostname catalogue
+
+HEAD "Install NodeJs"
+yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Add Roboshop App User"
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo User is already there, So Skipping the User creation &>>/tmp/roboshop.log
+  STAT $?
+  else
+    useradd roboshop &>>/tmp/roboshop.log
+    STAT $?
+    fi
+
+HEAD "Download App from GitHub"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract the Downloaded Archive"
+cd /home/roboshop/catalogue && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+STAT $?
+
+HEAD "Install NodeJS Dependencies"
+cd /home/roboshop/catalogue && npn install --unsafe-perm &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Fix Permissions to App content"
+chown roboshop:roboshop /home/roboshop -R
+STAT $?
+
