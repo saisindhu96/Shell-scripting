7acf7a4c6777943d0e3b05ad6b843e0d22690c75
saisindhu96 <saisindhuraju@gmail.com>
2021-06-13 11:35:15 +0530

    added frontend  file

--- 08.if.sh
+++ 08.if.sh
@@ -9,3 +9,13 @@ if [ "$username" == "root" ]; then
 else
   echo "Hey, User $username is a Normal User"
 fi
+
+read -p 'Enter filename: ' filename
+
+if [ -f "$filename" ]; then
+  echo "File Exists"
+else
+  echo "File Not Found"
+fi
+
+## Note: Always try to use variable inside quotes in expressions
