18f2c5409e4fe0832cfa99df4e80c040e24ebcc2
saisindhu96 <saisindhuraju@gmail.com>
2021-06-13 21:49:28 +0530

    added

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -18,7 +18,7 @@ DNS_UPDATE() {
   aws route53 change-resource-record-sets --hosted-zone-id Z0425287CYMKQ4RMER3W--change-batch file:///tmp/record.json | jq
 }
 
-INSTANCE_CREATE() {
+
   INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
   if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "${COMPONENT} Instance already exists!!"
