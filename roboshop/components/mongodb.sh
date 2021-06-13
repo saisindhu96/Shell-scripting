71ef0eb59e40725ef90f0d2c63382a0b424505b5
saisindhu96 <saisindhuraju@gmail.com>
2021-06-13 19:52:52 +0530

    added

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -10,18 +10,19 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-STAT &?
+STAT $?
 
 HEAD "Install MongoDB\t\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
-STAT &?
+STAT $?
 
 
 
-Head "Start MongoDB Service\t\t"
+HEAD "Start MongoDB Service\t\t"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl start mongod &>>/tmp/roboshop.log
-STAT &?
+STAT $?
+
 
 
 
