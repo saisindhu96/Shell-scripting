92685aca6dce8066cf6130a3db93bfb79c485a08
saisindhu96 <saisindhuraju@gmail.com>
2021-06-18 12:59:39 +0530

    added

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,40 +3,4 @@
 source components/common.sh
 rm -f /tmp/roboshop.log
 
-HEAD "Install NodeJs"
-yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
-STAT $?
-
-HEAD "Add Roboshop App User"
-id roboshop &>>/tmp/roboshop.log
-if [ $? -eq 0 ]; then
-  echo User is already there, So Skipping the User creation &>>/tmp/roboshop.log
-  STAT $?
-  else
-    useradd roboshop &>>/tmp/roboshop.log
-    STAT $?
-    fi
-
-HEAD "Download App from GitHub"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
-STAT $?
-
-HEAD "Extract the Downloaded Archive"
-cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
-STAT $?
-
-HEAD "Install NodeJS Dependencies"
-cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
-STAT $?
-
-HEAD "Fix Permissions to App content"
-chown roboshop:roboshop /home/roboshop -R
-STAT $?
-
-HEAD "Setup Systemd service"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-STAT $?
-
-HEAD "Start catalogue service"
-systemctl daemon-reload && systemctl enable catalogue && systemctl restart catalogue &>>/tmp/roboshop.log
-STAT $?
+NODEJS "catalogue"
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -15,3 +15,45 @@ STAT() {
     exit 1
   fi
 }
+APP_USER_ADD(){
+  HEAD "Add Roboshop App User"
+  id roboshop &>>/tmp/roboshop.log
+  if [ $? -eq 0 ]; then
+  echo User is already there, So Skipping the User creation &>>/tmp/roboshop.log
+  STAT $?
+  else
+    useradd roboshop &>>/tmp/roboshop.log
+    STAT $?
+    fi
+ }
+NODEJS() {
+HEAD "Install NodeJs"
+yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Download App from GitHub"
+curl -s -L -o /tmp/$1.zip "https://github.com/roboshop-devops-project/$1/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+APP_USER_ADD
+
+HEAD "Extract the Downloaded Archive"
+cd /home/roboshop && rm -rf $1 && unzip /tmp/$1.zip &>>/tmp/roboshop.log && mv $1-main $1
+STAT $?
+
+HEAD "Install NodeJS Dependencies"
+cd /home/roboshop/$1 && npm install --unsafe-perm &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Fix Permissions to App content"
+chown roboshop:roboshop /home/roboshop -R
+STAT $?
+
+HEAD "Setup Systemd service"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
+STAT $?
+
+HEAD "Start $1 service"
+systemctl daemon-reload && systemctl enable $1 && systemctl restart $1 &>>/tmp/roboshop.log
+STAT $?
+}
