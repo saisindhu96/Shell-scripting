d5d844e283e83924f947dffb44c7ac49b1703494
saisindhu96 <saisindhuraju@gmail.com>
2021-06-18 10:35:21 +0530

    added

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -12,26 +12,18 @@ LID=lt-0dffc04a2621e4a5f
 LVER=1
 
 ## Validate If Instance is already there
-DNS_UPDATE() {
-  PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
-  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0425287CYMKQ4RMER3W --change-batch file:///tmp/record.json | jq
-}
 
 
   INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
   if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "Instance already exists!!"
-  DNS_UPDATE
     exit 0
   fi
 
   if [ "${INSTANCE_STATE}" = "stopped" ]; then
     echo "Instance already exists!!"
-    DNS_UPDATE
     exit 0
   fi
 
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
-  sleep 10
-  DNS_UPDATE
+
