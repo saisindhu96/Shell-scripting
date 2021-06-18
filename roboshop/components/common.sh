9050e63b818d784fb640a50b748cc06605fec4f3
saisindhu96 <saisindhuraju@gmail.com>
2021-06-18 17:36:43 +0530

    added

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -78,7 +78,5 @@ MAVEN() {
   cd /home/roboshop/$1 && mvn clean package &>> /tmp/roboshop.log && mv target/$1-1.0.jar $1.jar  &>>/tmp/roboshop.log
   STAT $?
 
-  FIX_APP_CONENT_PERM
-
   SETUP_SYSTEMD "$1"
 }
