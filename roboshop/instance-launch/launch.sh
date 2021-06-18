0dde82885980d4059f53e07ddef92ac3ec32644c
saisindhu96 <saisindhuraju@gmail.com>
2021-06-18 11:37:24 +0530

    added

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -33,7 +33,7 @@ DNS_UPDATE() {
 
 
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
-  sleep 10
+  sleep 30
   DNS_UPDATE
 
 
